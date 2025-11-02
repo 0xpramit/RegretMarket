0796e52a97ced3597eb79d6255047b8707cb9563
0xpramit <pramitx2001@proton.me>
2025-11-02 21:04:49 +0545

    updating vite config for vercel deployment

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -4,15 +4,40 @@ import { nodePolyfills } from 'vite-plugin-node-polyfills'
 import viteTsconfigPaths from 'vite-tsconfig-paths'
 import { resolve } from 'node:path'
 
-
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [
-		nodePolyfills({}),
+		nodePolyfills({
+			include: ['buffer', 'process', 'util', 'stream', 'events'],
+			globals: {
+				Buffer: true,
+				global: true,
+				process: true,
+			},
+		}),
 		react(),
 		viteTsconfigPaths({
-			//
 			root: resolve(__dirname),
 		}),
 	],
+	resolve: {
+		alias: {
+			'rpc-websockets': 'rpc-websockets/dist/lib/client/websocket.browser.js'
+		}
+	},
+	optimizeDeps: {
+		include: ['rpc-websockets', '@solana/web3.js'],
+		esbuildOptions: {
+			target: 'esnext',
+		}
+	},
+	build: {
+		target: 'esnext',
+		commonjsOptions: {
+			transformMixedEsModules: true
+		},
+		rollupOptions: {
+			external: [],
+		}
+	}
 })
